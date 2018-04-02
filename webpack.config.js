0b1d27b7eee1d277c17bef2612d0d6ad30fb7b8c
James Sullivan <jamesfsullivan5@gmail.com>
2018-04-01 20:38:19 -0600

    Add vendor app code splitting in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,39 @@ const config = {
       path.resolve('./node_modules')
     ]
   },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  // entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby',
+    ],
+    app: ['./lib/renderers/dom.js']
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
     ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          test: path.resolve(__dirname, 'node_modules'),
+          name: 'vendor',
+          enforce: true
+        }
+      }
+    }
   }
 };
 
